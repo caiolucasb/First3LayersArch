5af5838ed0b6fc589d07b0e8b0ebf1bd07183a43
caiolucasb <caio.lucas75@gmail.com>
2022-03-29 04:25:20 -0300

    IT-02: Creates DTO of orders

---
+++ IST/IST.BLL/DTO/InboundOrder.cs
@@ -0,0 +1,8 @@
+﻿namespace IST.BLL.DTO
+{
+    public class InboundOrder
+    {
+        public float Price { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
---
+++ IST/IST.BLL/DTO/OutboundOrder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IST.BLL.DTO
+{
+    public class OutboundOrder
+    {
+        public int Id { get; set; }
+        public float Price { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
