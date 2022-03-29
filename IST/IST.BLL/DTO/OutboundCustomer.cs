dec874503c6a05cfc428fd056ac86887a234486a
caiolucasb <caio.lucas75@gmail.com>
2022-03-29 04:25:05 -0300

    IT-02: Creates DTO of customers

---
+++ IST/IST.BLL/DTO/OutboundCustomer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace IST.BLL.DTO
+{
+    public class OutboundCustomer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class OutboundCustomerWithOrders : OutboundCustomer
+    {
+        public List<OutboundOrder> Orders { get; set; }
+    }
+}
