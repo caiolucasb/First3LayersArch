ba93295febd98e72553ca88d7e8b228e6ea7f851
caiolucasb <caio.lucas75@gmail.com>
2022-03-29 04:28:40 -0300

    IT-02: Creates the BllMethos to acess the Data acess layer

---
+++ IST/IST.BLL/Services/AcessDAL.cs
@@ -0,0 +1,45 @@
+﻿using IST.BLL.DTO;
+using IST.DAL.Repository;
+using IST.BLL.FactoryDTO;
+using System.Collections.Generic;
+
+namespace IST.BLL.Services
+{
+    public class AcessDAL
+    {
+        private readonly DbAcess _db;
+        private readonly CustomerFactory _customerFactory;
+        private readonly OrderFactory _orderFactory;
+        public AcessDAL()
+        {
+            _db = new DbAcess();
+            _customerFactory = new CustomerFactory();
+            _orderFactory = new OrderFactory();
+        }
+
+        public void CreateACustomerBLL(InboundCustomer inbound)
+        {
+            var customer = _customerFactory.InboundCustomerToEntity(inbound);
+            _db.AddANewCustomer(customer);
+        }
+
+        public OutboundCustomerWithOrders GetACustomerBLL(int id)
+        {
+            var customer = _db.GetACustomer(id);
+            customer.Orders = _db.GetOrdersByCustomerId(id);
+            return _customerFactory.EntityToOutboundCustomerWithOrders(customer);
+        }
+
+        public List<OutboundCustomer> GetAllCustomersBLL()
+        {
+            var customersList = _db.GetAllCustomers();
+            return _customerFactory.EntitiesToListOfOutboundCustomers(customersList);
+        }
+
+        public void CreateAOrder(InboundOrder inbound)
+        {
+            var order = _orderFactory.InboundOrderToEntity(inbound);
+            _db.AddANewOrder(order);
+        }
+    }
+}
