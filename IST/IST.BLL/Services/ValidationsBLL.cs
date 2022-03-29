55e9553739cce2edfc5a624ebd6e616782f37a8f
caiolucasb <caio.lucas75@gmail.com>
2022-03-29 14:24:58 -0300

    IT-04: Creates some validations in BLL

---
+++ IST/IST.BLL/Services/ValidationsBLL.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.RegularExpressions;
+using System.Net.Mail;
+
+namespace IST.BLL.Services
+{
+    public class ValidationsBLL
+    {
+        public bool IsValidEmail(string email)
+        {
+            var trimmedEmail = email.Trim();
+
+            if (trimmedEmail.EndsWith("."))
+                return false;
+
+            var addr = new MailAddress(email);
+            if (addr.Address != trimmedEmail)
+                return false;
+
+            return true;
+        }
+
+        public bool IsValidName(string name)
+        {
+            Regex regex = new Regex(
+              @"^[a-zA-Z]+$",
+            RegexOptions.IgnoreCase
+            | RegexOptions.CultureInvariant
+            | RegexOptions.IgnorePatternWhitespace
+            | RegexOptions.Compiled
+            );
+
+           if(regex.IsMatch(name))
+                return true;
+           else
+                return false;
+        }
+        public bool IsAValidId(int id)
+        {
+            if(id <= 0)
+                return false;
+
+            return true;
+        }
+    }
+}
