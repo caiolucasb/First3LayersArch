ff3ced30e77b1f869d56875e919fc925af711b44
caiolucasb <caio.lucas75@gmail.com>
2022-04-01 03:10:21 -0300

    IT-08: Substitute the hardcoded connection string for the appsettings one

--- IST/IST.DAL/Data/CustomerDbContext.cs
+++ IST/IST.DAL/Data/CustomerDbContext.cs
@@ -1,16 +1,25 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
 using IST.DAL.Entities;
+using IST.DAL.AppSettings;
+using System;
 
 namespace IST.DAL.Data
 {
     public class CustomerDbContext : DbContext
     {
+        readonly ConnectionString _settings;
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Order> Orders { get; set; }
 
+        public CustomerDbContext(IOptions<ConnectionString> settings)
+        {
+            _settings = settings.Value;
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=CustomerApp;Data Source=DESKTOP-CKBOR6V");
+            optionsBuilder.UseSqlServer(_settings.MsSqlConnectionString);
         }
     }
 }
